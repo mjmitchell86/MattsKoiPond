aeef374c1612deee4e386320e14aa9518e8c79e4
Matt Mitchell <mjmitchell86@gmail.com>
2020-06-12 19:44:30 -0400

    Udpdate Times in Titles / Summer hours (#4)

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,11 +1,7 @@
 <template>
   <v-layout>
     <v-flex class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
+      <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="dcdc6442-0943-4457-9fa0-3b428d86a08b" data-share-badge-host="https://www.youracclaim.com"></div><script type="text/javascript" async src="//cdn.youracclaim.com/assets/utilities/embed.js"></script>
       <blockquote class="blockquote">
         &#8220;First, solve the problem. Then, write the code.&#8221;
         <footer>
